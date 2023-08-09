d1e6d77505534cab9d515c0e04edf68b6c616f9e
Oscar5323 <jr.mercado2001@gmail.com>
2023-08-09 13:14:36 -0500

    start function

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-const inquirer = require('inquirer')
-const mysql = require('mysql2')
-const login = require('./secrets')
-import{user,password}from './secrets'
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+import{user,password}from './secrets';
 
-const connect = mysql.createConnection({
+const connects = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: user,
     password: password,
-})
+});
+
+connects.connect((err)=>{
+    if (err) throw err;
+    console.log('connected to data');
+    start()
+});
+
+function start(){
+    inquirer
+        .prompt({
+            type:'list',
+            name:'action',
+            message:'pick an option',
+            choices:[
+                'View all departments',
+                'View all employees',
+                'View all roles',
+                'Add departments',
+                'Add employee',
+                'Add role',
+                'Delete',
+                'Exit'
+            ]
+        })
+        .then((answers)=>{
+
+        });
+};
